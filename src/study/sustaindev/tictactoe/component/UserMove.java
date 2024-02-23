fdcfcbea538fe43f824acbf5e8128ebfc200b40d
Aleksandr <edmams789@gmail.com>
2024-02-23 10:59:42 +0200

    Implement UserMove component

--- src/study/sustaindev/tictactoe/component/UserMove.java
+++ src/study/sustaindev/tictactoe/component/UserMove.java
@@ -16,7 +16,10 @@
 
 package study.sustaindev.tictactoe.component;
 
+import study.sustaindev.tictactoe.model.Cell;
 import study.sustaindev.tictactoe.model.GameTable;
+
+import java.util.Scanner;
 
 /**
  * @author edmams789
@@ -24,7 +27,40 @@ import study.sustaindev.tictactoe.model.GameTable;
  */
 public class UserMove {
 
-    public void make(final GameTable gameTable) {
+    private final char[][] mapping = {
+            {'7', '8', '9'},
+            {'4', '5', '6'},
+            {'1', '2', '3'}
+    };
 
+    public void make(final GameTable gameTable) {
+        while (true) {
+            final Cell cell = getUserInput();
+            if (gameTable.isEmpty(cell)) {
+                gameTable.setSign(cell, 'X');
+                return;
+            } else {
+                System.out.println("Can't make a move, because the cell is not free! Try again!");
+            }
+        }
+    }
+
+    private Cell getUserInput() {
+        while (true) {
+            System.out.println("Please type number between 1 and 9:");
+            final String userInput = new Scanner(System.in).nextLine();
+            if (userInput.length() == 1) {
+                final char ch = userInput.charAt(0);
+                if (ch >= '1' && ch <= '9') {
+                    for (int i = 0; i < 3; i++) {
+                        for (int j = 0; j < 3; j++) {
+                            if (mapping[i][j] == ch) {
+                                return new Cell(i, j);
+                            }
+                        }
+                    }
+                }
+            }
+        }
     }
 }
