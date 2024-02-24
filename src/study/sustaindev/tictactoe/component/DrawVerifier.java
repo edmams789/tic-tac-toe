a3216065a12bd859a78880894f8772920f672d69
Aleksandr <edmams789@gmail.com>
2024-02-24 08:52:05 +0200

    Implement DrawVerifier component

--- src/study/sustaindev/tictactoe/component/DrawVerifier.java
+++ src/study/sustaindev/tictactoe/component/DrawVerifier.java
@@ -16,6 +16,7 @@
 
 package study.sustaindev.tictactoe.component;
 
+import study.sustaindev.tictactoe.model.Cell;
 import study.sustaindev.tictactoe.model.GameTable;
 
 /**
@@ -25,6 +26,13 @@ import study.sustaindev.tictactoe.model.GameTable;
 public class DrawVerifier {
 
     public boolean isDraw(final GameTable gameTable) {
-        return false;
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (gameTable.isEmpty(new Cell(i, j))) {
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 }
